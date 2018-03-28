1857d63e901e2cc9906a414c51c42acaace0a606
beom <beommaker@gmail.com>
2018-03-28 17:59:01 +0900

    feat(button): create-button-component

--- components/Timer/index.js
+++ components/Timer/index.js
@@ -1,45 +1,44 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
-
+import React, { Component } from "react";
+import { View, StyleSheet, Text, StatusBar } from "react-native";
+import Button from "../Button";
 
 class Timer extends Component {
     render() {
         return (
             <View style={styles.container}>
-            <StatusBar barStyle={"light-content"} />
+                <StatusBar barStyle={"light-content"} />
                 <View style={styles.upper}>
                     <Text style={styles.time}>25:00</Text>
                 </View>
                 <View style={styles.lower}>
-                    <Text>BUTTONS HERE</Text>
+                    <Button iconName={"play-circle"} onPress={() => alert('it works!')} />
+                    <Button iconName={"stop-circle"} onPress={() => alert('it works!')} />
                 </View>
             </View>
-        )
+        );
     }
 }
-
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#000000",
+        backgroundColor: "#000000"
     },
     upper: {
         flex: 2,
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: "center"
     },
     lower: {
         flex: 1,
-        justifyContent: "center",
         alignItems: "center",
+        justifyContent: "center",
     },
     time: {
         color: "white",
         fontSize: 120,
-        fontWeight: "100",
+        fontWeight: "100"
     }
 });
-
 
 export default Timer;
